d6b4fae93041c82347d69d750258abbacbec10df
Óscar Morales Vivó <oscarmv@mac.com>
2023-06-04 17:08:11 -0700

    Added support for tvOS

--- Package.swift
+++ Package.swift
@@ -8,26 +8,23 @@ let package = Package(
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),
-        .macCatalyst(.v13)
+        .macCatalyst(.v13),
+        .tvOS(.v13)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AutoLayoutHelpers",
-            targets: ["AutoLayoutHelpers"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["AutoLayoutHelpers"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AutoLayoutHelpers",
-            dependencies: []),
+            dependencies: []
+        ),
         .testTarget(
             name: "AutoLayoutHelpersTests",
-            dependencies: ["AutoLayoutHelpers"]),
+            dependencies: ["AutoLayoutHelpers"]
+        )
     ]
 )
